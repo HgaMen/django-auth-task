41a25ad481f5414be0e78d059ba0f5e7172c3d89
HgaMen <hga.int2021@gmail.com>
2026-01-23 11:11:25 -0600

    Personalizar

--- djangocrud/settings.py
+++ djangocrud/settings.py
@@ -87,10 +87,6 @@ DATABASES = {
         default='postgresql://postgres:postgres@localhost/postgres',
         conn_max_age=600
     )
-    # {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
 }
 
 
